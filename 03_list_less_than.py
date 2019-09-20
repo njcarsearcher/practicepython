b184e82db6eaac99442268ccce90fe493892dc7f
NJ Carsearcher <njcarsearcher@gmail.com>
2019-09-19 22:27:11 -0400

    Changes to list_less_than file 	modified:   03_list_less_than.py

--- 03_list_less_than.py
+++ 03_list_less_than.py
@@ -13,3 +13,17 @@ for element in a:
 	if element < numbercheck:
 		print (f'{element}', end=" ")
 
+#tip - if you want to print on same line, use a comma at the end of print statement
+#tip - to print a variable, use {0} {1} ... with .format(elem1,elem2, ...)
+#		print ('{0} '.format(element), end=" ")
+
+#tip - range (2,11,2) - returns a list of even numbers from 2 to 11
+print('\n Printing Range of numbers:', end=" ")
+for i in range(2,11,2):
+	print(i, end=" ")
+
+print ("\nNew Example\n\n")
+my_list = [1,3, "Michele", [5,6,7]]
+print (*my_list)
+for elem in my_list:
+	print(elem)
