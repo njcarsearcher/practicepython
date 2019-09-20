95d92d338e605457f0eaa73b8bf11fd6e9de9b45
NJ Carsearcher <njcarsearcher@gmail.com>
2019-09-19 22:02:33 -0400

    changing char_input program, adding new odd_even checking program 	modified:   01_char_input.py

--- 01_char_input.py
+++ 01_char_input.py
@@ -3,3 +3,7 @@
 name = input("Give me your name: ")
 print("Your name is " + name)
 
+age = input("How old are you: ")
+futureage = int(age) + 5
+print ("In 5 years you will be " + str(futureage))
+
